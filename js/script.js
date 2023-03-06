067671b859d29179791de467fbcbf973a663f313
Pranjali <pranjali.bisht711@gmail.com>
2023-03-06 23:02:24 +0530

    recommendation system

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function form_validate() {
+    var gender = document.querySelector('#gender');
+    var age = document.querySelector('#age');
+    var cusine = document.querySelector('#cusine');
+    var price = document.querySelector('#price');
+    var rating = document.querySelector('#rating');
+    var v = Packages.com.js.p1.hello();
+    alert(v);
+}
